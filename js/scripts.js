e24f3124d795fadb1f170e7c864bdde89d344ba6
Ivan <ivan.frontcoder@gmail.com>
2022-07-20 14:41:05 +0500

    .

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,31 @@
 window.onload = function () {
 
+  // Swiper
+  if ($('#swiper').length) {
+    const swiper = new Swiper('#swiper', {
+      // Optional parameters
+
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+      },
+
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+      // And if we need scrollbar
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
+    });
+  }
+
+  // Air Datepicker
+  new AirDatepicker('#airDatepicker', {
+    position: 'right top',
+  });
+
 }
