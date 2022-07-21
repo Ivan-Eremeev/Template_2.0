c1ba1b2352af535752832034cb328bcdee0bdf8e
Ivan <ivan.frontcoder@gmail.com>
2022-07-21 22:22:58 +0500

    .

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ window.onload = function () {
   if ($('#swiper').length) {
     const swiper = new Swiper('#swiper', {
       // Optional parameters
+      slidesPerView: 1,
 
       // If we need pagination
       pagination: {
@@ -32,5 +33,33 @@ window.onload = function () {
   $('.open-popup-link').magnificPopup({
     mainClass: 'mfp-fade'
   });
+
+  // Табы
+	function tabs() {
+    const tabs = $('.js-tabs');
+		if (tabs.length) {
+			tabs.each( function () {
+        let triggers = $(this).find('.js-tabs-trigger');
+        let contents = $(this).find('.js-tabs-content');
+        let time = 300;
+        triggers.on('click', function () {
+          let trigger = $(this);
+          let content = $('.js-tabs-content[data-href="' + trigger.attr('href') +'"]');
+          if (!trigger.hasClass('active')) {
+            triggers.removeClass('active');
+            trigger.addClass('active');
+            contents.hide();
+            contents.removeClass('open');
+            content.fadeIn(time, function () {
+              $(this).addClass('open');
+            });
+          }else {
+            return false;
+          }
+        })
+      });
+		}
+	}
+	tabs();
 
 }
