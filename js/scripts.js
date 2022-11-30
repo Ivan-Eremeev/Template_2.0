8808b25ab7c5c3f68bb40c1a06d7baf2c2254cc4
Ivan <ivan.frontcoder@gmail.com>
2022-11-30 22:43:24 +0500

    .

--- js/scripts.js
+++ js/scripts.js
@@ -20,22 +20,16 @@ window.onload = function () {
   // Swiper | Слайдер
   if ($('#swiper').length) {
     const swiper = new Swiper('#swiper', {
-      // Optional parameters
       slidesPerView: 1,
-
-      // If we need pagination
+      simulateTouch: false,
       pagination: {
         el: '.swiper-pagination',
         clickable: true,
       },
-
-      // Navigation arrows
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
       },
-
-      // And if we need scrollbar
       scrollbar: {
         el: '.swiper-scrollbar',
       },
