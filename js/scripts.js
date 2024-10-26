696b4911af141ed875a9d9cd8fc33eb56d7f2e35
Ivan <ivan.frontcoder@gmail.com>
2024-10-26 19:12:55 +0400

    .

--- js/scripts.js
+++ js/scripts.js
@@ -459,4 +459,8 @@ window.onload = function () {
   const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
   const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+  $('#pagepiling').pagepiling({
+    verticalCentered: false,
+  });
+
 }
